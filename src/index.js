717dbe36bc3603a27201f43c564b41d9c1c959da
Joshua <s3687337@student.rmit.edu.au>
2021-08-24 00:08:18 +1000

    initial commit

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import './qfh_css.min.css'
 
 ReactDOM.render(
   <React.StrictMode>
